b251dc545b809fd1fda76498d8af995c33f5a430
Titouan Bénard <itotutona.github@evta.fr>
2021-09-05 11:59:10 +0200

    Update apps.py

--- {{cookiecutter.app_slug}}/{{cookiecutter.app_slug}}/apps.py
+++ {{cookiecutter.app_slug}}/{{cookiecutter.app_slug}}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{ cookiecutter.app_slug.replace("_", " ").capitalize().replace(" ", "") }}Config(AppConfig):
+class {{ cookiecutter.app_slug.replace("_", " ").replace("-", " ").capitalize().replace(" ", "") }}Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = '{{ cookiecutter.app_slug }}'
